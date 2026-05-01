bfa103b61fd93b4e2b01b23dde580d03b5f08c48
Santiago Lazcano <santiago.lazcano@ucb.edu.bo>
2026-04-30 23:10:49 -0400

    arreglo filtros

--- FinTrack.Infraestructure/obj/Debug/net10.0/FinTrack.Infraestructure.AssemblyInfo.cs
+++ FinTrack.Infraestructure/obj/Debug/net10.0/FinTrack.Infraestructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinTrack.Infraestructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2298ccfbc390714fca27683daa8a760552809100")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+693829a318e25ba6b21fce1823509a6e4524a993")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinTrack.Infraestructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinTrack.Infraestructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
